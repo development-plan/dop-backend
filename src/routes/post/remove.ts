01e9de4fbc7d7289b6fe48542f18ff0accf5b385
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-28 07:36:21 +0900

    feat: post remove endpoint

--- src/routes/post/remove.ts
+++ src/routes/post/remove.ts
@@ -1,5 +1,17 @@
 import express, { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import { authMiddleware, IAuthRequest } from '../../middlewares/auth';
+import postModel from '../../models/postModel';
 
 const router: express.IRouter = Router();
+
+router.use('/:postID', authMiddleware);
+router.delete('/:postID', expressAsyncHandler(
+  async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
+    const id: string = req.params.postID;
+    await postModel.findByIdAndDelete(id);
+    return res.json({});
+  }),
+);
 
 export default router;
