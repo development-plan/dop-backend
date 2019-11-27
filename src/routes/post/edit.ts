053588c839d55499c0dd72c695699ef266e808e6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-28 08:04:09 +0900

    feat: post edit endpoint

--- src/routes/post/edit.ts
+++ src/routes/post/edit.ts
@@ -1,5 +1,21 @@
 import express, { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import { authMiddleware, IAuthRequest } from '../../middlewares/auth';
+import postModel, { IPostPayload } from '../../models/postModel';
 
 const router: express.IRouter = Router();
+
+router.use('/:postID', authMiddleware);
+router.put('/:postID', expressAsyncHandler(
+  async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
+    const id: string = req.params.postID;
+    const { content, images, tags, title }: IPostPayload = req.body;
+
+    await postModel.findOneAndUpdate({ _id: id }, {
+      content, images, tags, title,
+    });
+    return res.json({});
+  }),
+);
 
 export default router;
