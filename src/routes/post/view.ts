4dd02e93053a69a47678a3b8ceffa63899e6f403
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-28 22:05:09 +0900

    feat: query author and answers when view post

--- src/routes/post/view.ts
+++ src/routes/post/view.ts
@@ -1,7 +1,11 @@
 import express, { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+
 import { authMiddleware, IAuthRequest } from '../../middlewares/auth';
+
+import answerModel from '../../models/answerModel';
 import postModel, { IPostModel } from '../../models/postModel';
+import userModel, { IUserModel } from '../../models/userModel';
 
 const router: express.IRouter = Router();
 
@@ -10,7 +14,13 @@ router.use('/', authMiddleware);
 router.get('/', expressAsyncHandler(
   async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
     const posts: any = await postModel.find({});
-    return res.json({ posts });
+    return res.json({
+      posts: await posts.map(async (post: IPostModel) => {
+        const author: IUserModel = await userModel.findById(post.author);
+        const answers: any = await answerModel.find({ post: post.id });
+        return { ...post, author, answers };
+      }),
+    });
   }),
 );
 
@@ -20,7 +30,11 @@ router.get('/:postID', expressAsyncHandler(
   async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
     const id: string = req.params.postID;
     const post: IPostModel = await postModel.findById({ id });
-    return res.json({ post });
+    const author: IUserModel = await userModel.findById(post.author);
+    const answers: any = await answerModel.find({ post: id });
+    return res.json({
+      post: { ...post, author, answers },
+    });
   }),
 );
 
