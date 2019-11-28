ade58647526a45e61b268c4f41491b78038c24ed
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-28 20:45:32 +0900

    feat: answer like endpoint

--- src/routes/answer/like.ts
+++ src/routes/answer/like.ts
@@ -9,6 +9,19 @@ router.use('/:answerID/like', authMiddleware);
 router.post('/:answerID/like', expressAsyncHandler(
   async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
     const answerID: string = req.params.answerID;
+    const answer: IAnswerModel = await answerModel.findById(answerID);
+
+    const { id: userID }: { id: string } = req.identity;
+
+    await answerModel.update(
+      { _id: answerID },
+      {
+        [answer.likes.includes(userID) ? '$pull' : '$push']: {
+          likes: userID,
+        },
+      },
+    );
+
     return res.json({});
   }),
 );
