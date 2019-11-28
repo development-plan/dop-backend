5f7a83ed3725646ae052d3019053a0e297cad6c5
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-28 21:16:45 +0900

    feat: answer edit endpoint

--- src/routes/answer/edit.ts
+++ src/routes/answer/edit.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import { authMiddleware, IAuthRequest } from '../../middlewares/auth';
-import answerModel, { IAnswerModel } from '../../models/answerModel';
+import answerModel, { IAnswerPayload } from '../../models/answerModel';
 
 const router: express.IRouter = Router();
 
@@ -9,6 +9,11 @@ router.use('/:answerID', authMiddleware);
 router.put('/:answerID', expressAsyncHandler(
   async (req: IAuthRequest, res: express.Response, _: express.NextFunction) => {
     const answerID: string = req.params.answerID;
+    const { content, schedule }: IAnswerPayload = req.body;
+
+    await answerModel.findOneAndUpdate({ _id: answerID }, {
+      content, schedule,
+    });
     return res.json({});
   }),
 );
