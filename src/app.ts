d1f1391ccb0abee068d77c6066560543dc15677f
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-29 10:51:45 +0900

    feat: serve static as uploads

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 app.use(auth().initialize());
 app.use('/', routes);
